2032e03d7918aaab5dfbc031ad55f6dc54dbf41a
KNssSuraj <knsssuraj16@gmail.com>
2021-05-29 11:34:36 +0530

    add question

---
+++ 4 Count binery String/code.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int p0 =1;
+        int p1 =1;
+        int c0=0;
+        int c1=0;
+        for(int i =2 ; i<= n; i++){
+            c1 = p0+p1;
+            c0 =p1;
+            p1 =c1;
+            p0 =c0;
+        }
+        System.out.println(c1+c0);
+ }
+
+}
---
+++ 4 Count binery String/readme.md
@@ -0,0 +1,3 @@
+1. You are given a number n.
+2. You are required to print the number of binary strings of length n with no consecutive 0's.
+
