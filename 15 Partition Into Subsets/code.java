18b669b8def2bfa3d97ae4ac5f0c3dd325abe610
KNssSuraj <knsssuraj16@gmail.com>
2021-05-31 11:13:20 +0530

    day 3 31 may 2021 update 3

---
+++ 15 Partition Into Subsets/code.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+        
+    public static long partitionKSubset(int n, int k) {
+        long [][]dp = new long[k+1][n+1];
+        
+        for(int  i= 1; i<=k; i++){
+            for(int j = 1 ; j<=n; j++){
+                if(j<i ){
+                    dp[i][j] =0;
+                    
+                }else if (i == j){
+                    
+                    dp[i][j] =1;
+                }else {
+                    dp[i][j] = (dp[i][j-1] * i)+ dp[i-1][j-1];
+                }
+            }
+        }
+        return dp[k][n];
+        
+        
+        
+    }
+    
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int k = scn.nextInt();
+        
+        long res = partitionKSubset(n, k);
+        System.out.println(res);
+    }
+}
