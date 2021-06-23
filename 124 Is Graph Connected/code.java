57381aa028b40b2eb94771778b6c232c25006931
KNssSuraj <knsssuraj16@gmail.com>
2021-06-23 23:29:50 +0530

    day 25 23 june update

---
+++ 124 Is Graph Connected/code.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt) {
+         this.src = src;
+         this.nbr = nbr;
+         this.wt = wt;
+      }
+   }
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+
+      System.out.println(checkgraph(graph,0,vtces-1,new boolean [vtces]));
+   }
+   
+   public static boolean checkgraph(ArrayList<Edge>[] graph,int src,int dest ,boolean [] visited){
+       
+       if(src == dest){
+           return true;
+       }
+       
+       visited[src] = true;
+       
+       for(Edge e : graph[src]){
+           if( visited[e.nbr] == false)
+           if(checkgraph(graph,e.nbr,dest,visited) == true){
+               return true;
+           }
+       }
+       
+       return false;
+       
+   }
+   
+   
+}
---
+++ 124 Is Graph Connected/readme.md
@@ -0,0 +1,4 @@
+1. You are given a graph.
+2. You are required to find and print if the graph is connected (there is a path from 
+     every vertex to every other).
+
