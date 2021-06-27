7295efe9396307b28f07345f59e64325528e36e1
KNssSuraj <knsssuraj16@gmail.com>
2021-06-27 11:43:11 +0530

    day 27 27 june update

---
+++ 127 Hamiltonian Path And Cycle/code.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt) {
+         this.src = src;
+         this.nbr = nbr;
+         this.wt = wt;
+      }
+   }
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+
+      int src = Integer.parseInt(br.readLine());
+
+    boolean []b= new boolean[graph.length];
+     findHamilton(graph,src,b,src,graph.length,"");
+   }
+
+    public static void  findHamilton(ArrayList<Edge>[] graph,int src, boolean []visited,int fsrc,int count,String psf){
+        
+        
+        count--;
+        if(count == 0){
+            boolean check = true;
+             for(Edge e:graph[src]){
+                 
+                    if(e.nbr == fsrc){
+                        check =false;
+                    }
+                }
+                if(check ==true){
+                    
+                    System.out.println(psf+src+".");
+                }else{
+                    
+                    System.out.println(psf+src+"*");
+                }
+        }
+        visited[src]= true;
+        
+        for(Edge e:graph[src]){
+            if(visited[e.nbr] == false)
+            findHamilton(graph,e.nbr,visited,fsrc,count,psf+src);
+        }
+        
+        visited[src]= false;
+    }
+
+}
---
+++ 127 Hamiltonian Path And Cycle/readme.md
@@ -0,0 +1,6 @@
+1. You are given a graph and a src vertex.
+2. You are required to find and print all hamiltonian paths and cycles starting from src. The cycles must end with "*" and paths with a "."
+
+Note -> A hamiltonian path is such which visits all vertices without visiting any twice. A hamiltonian path becomes a cycle if there is an edge between first and last vertex.
+Note -> Print in lexicographically increasing order.
+
