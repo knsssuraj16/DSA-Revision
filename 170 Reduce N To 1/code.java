34b9dd59f4965e0dfcb9048a42c986ac93429423
KNssSuraj <knsssuraj16@gmail.com>
2021-07-09 12:39:25 +0530

    day 38 09 July update

---
+++ 170 Reduce N To 1/code.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int solution(long n) {
+        int res =0 ;
+        while(n != 1){
+            if((n & 1)==0){
+                n=n/2;
+            }else if( n == 3 ){
+                n = n-1;
+            }else if((n & 3) == 1){
+                n= n-1;
+            }else if ((n & 3) == 3){
+                n = n+1;
+            }
+            res++;
+        }
+        return res;
+    }
+    
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+        System.out.println(solution(n));
+    }
+	
+	
+}
---
+++ 170 Reduce N To 1/readme.md
@@ -0,0 +1,6 @@
+1. You are given a positive number N.
+2. You have to find the minimum number of operations required to convert N into 1.
+3. Operations allowed :
+     (i)  If n is even, you have to replace n with n/2.
+     (ii) If n is odd, you can replace n with either n-1 or n+1.
+
