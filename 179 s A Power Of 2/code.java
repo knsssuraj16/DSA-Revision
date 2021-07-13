53c21db11f2fc7ea8a4252b291e9a993e98e94a3
KNssSuraj <knsssuraj16@gmail.com>
2021-07-13 12:32:20 +0530

    day 40 13 July update

---
+++ 179 s A Power Of 2/code.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int mask = (n &(n-1));
+    if(mask == 0){
+        System.out.println(true);;
+    }else{
+        System.out.println(false);
+    }
+    
+  }
+
+}
---
+++ 179 s A Power Of 2/readme.md
@@ -0,0 +1,3 @@
+1. You are given a number n.
+2. You have to check whether it is a power of 2 or not.
+
