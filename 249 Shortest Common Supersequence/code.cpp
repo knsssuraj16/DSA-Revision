cdb7727440a1455b62eafd6852068ddf949722f0
KNssSuraj <knsssuraj16@gmail.com>
2021-08-31 21:27:46 +0530

    S3 08/31/2021

---
+++ 249 Shortest Common Supersequence/code.cpp
@@ -0,0 +1,20 @@
+ int shortestCommonSupersequence(string x, string y, int m, int n)
+    {
+       int dp[m+1][n+1];
+       
+       for(int i=0; i<= m; i++){
+           for(int j=0; j<=n; j++){
+            if(i ==0 || j==0){
+                dp[i][j] =0;
+            }else {
+                if(x[i-1] == y[j-1]){
+                    dp[i][j] = 1+ dp[i-1][j-1];
+                }else{
+                    dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+         }
+       }
+       
+       return ((m+n)-dp[m][n]);
+    }
