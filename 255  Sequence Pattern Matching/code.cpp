06f7578af1e453cafd07e9240ba7dd5d4dbbdb56
KNssSuraj <knsssuraj16@gmail.com>
2021-09-02 13:44:36 +0530

    S3 09/02/2021

---
+++ 255  Sequence Pattern Matching/code.cpp
@@ -0,0 +1,22 @@
+  bool isSubsequence(string s, string t) {
+        	   int m= s.length();
+		   int n= t.length();
+		   
+		   int dp[m+1][n+1];
+		   
+		   for(int i =0; i<=m; i++){
+		       for(int j =0; j<=n ;j++ ){
+		           if(i ==0 || j==0){
+		               dp[i][j] = 0;
+		           }else{
+		               if(s[i-1] == t[j-1]){
+		                   dp[i][j] = 1+dp[i-1][j-1];
+		               }else{
+		                     dp[i][j]=  max(dp[i-1][j],dp[i][j-1]);
+		               }
+		           }
+		       }
+		   }
+		   
+		 return m==dp[m][n];
+    }
