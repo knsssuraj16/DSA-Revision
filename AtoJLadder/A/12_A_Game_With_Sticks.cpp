58d2ecc30bb049e2dfbafb38869b0f02a8d9364a
KNssSuraj <knsssuraj16@gmail.com>
2021-12-22 13:14:17 +0530

    20/12/2021

---
+++ AtoJLadder/A/12_A_Game_With_Sticks.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl ("\n")
+#define pi (3.141592653589)
+#define mod 1e9+7
+
+#define float double
+
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+
+int32_t main(){
+    fast
+    
+    int m ,n;
+    cin >> m >> n;
+    if(min(m,n) % 2 ==0){
+        cout << "Malvika"<<endl;
+    }else{
+        
+        cout << "Akshat"<<endl;
+
+    }
+    return 0;
+}
